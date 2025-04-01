3422e2f5ea3e7f2a19ec396ca06efc979f44697a
DarryllRobinson <darryllrobinson@icloud.com>
2025-04-01 17:13:45 +1000

    initialize backend server with essential configurations and middleware

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const errorHandler = require("./_middleware/error-handler");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Helmet stuff to go here but I don't know enough about it yet
+// helmetjs.github.io/?ref=hackernoon.com
+// app.use(helmet());
+
+// allow cors requests from any origin and with credentials
+https: app.use(
+  cors({
+    origin: (origin, callback) => callback(null, true),
+    credentials: true,
+  })
+);
+
+// api routes
+app.use("/users", require("./users/users.controller"));
+
+// swagger docs route
+app.use("/api-docs", require("_helpers/swagger"));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+app.listen(port, () => console.log("Server listening on port " + port));
